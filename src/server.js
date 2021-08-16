69cc6f3bcfd5d4ee6ebfc3cdc65cb63b137178e5
Cullen Sharp <cullen.sharp100@gmail.com>
2021-08-16 16:16:40 -0700

    Adds configuration

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const cors = require('cors')
+const express = require('express')
+
+const app = express()
+app.use(cors())
+
+app.use('/*', (req, res) => {
+  res.status(404).send()
+})
+
+module.exports = {
+  app: app,
+  start: function (PORT) {
+    app.listen(PORT, console.log(`Alert, Awake, Alive ${PORT}`))
+  }
+}
