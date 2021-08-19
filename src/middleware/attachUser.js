5d96d95eb4e41f1daadf97c60ba84685803e5915
Louis Lassegue <louislassegue@gmail.com>
2021-08-18 21:19:47 -0700

    console.log test

--- src/middleware/attachUser.js
+++ src/middleware/attachUser.js
@@ -5,10 +5,12 @@ const { users } = require('../models/index.js')
 module.exports = async (req, res, next) => {
   const token = req.headers.authorization.split(' ')[1]
   const model = users.model
+  console.log('line 8', model)
 
   verifyToken(token, getUser)
 
   async function getUser (user) {
+    console.log('line 12', user)
     const userRecord = await model.find({ where: { email: user.email } })
     req.user = userRecord
     next()
