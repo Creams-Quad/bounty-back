5a3e35c4bf7fe985b9cdf2099b9a5630a9138192
Louis Lassegue <louislassegue@gmail.com>
2021-08-18 21:33:13 -0700

    Add try catch

--- src/middleware/attachUser.js
+++ src/middleware/attachUser.js
@@ -3,15 +3,16 @@ const verifyToken = require('./verifiedToken.js')
 const dataModule = require('../models/index.js')
 
 module.exports = async (req, res, next) => {
-  const token = req.headers.authorization.split(' ')[1]
-  const model = dataModule.users.model
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    const model = dataModule.users.model
 
-  verifyToken(token, getUser)
+    verifyToken(token, getUser)
 
-  async function getUser (user) {
-    const userRecord = await model.findOne({ where: { email: user.email } })
-    req.user = userRecord
-    next()
-  }
-  next('user not found')
+    async function getUser (user) {
+      const userRecord = await model.findOne({ where: { email: user.email } })
+      req.user = userRecord
+      next()
+    }
+  } catch (e) { next(e) }
 }
