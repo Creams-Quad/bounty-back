1ead266427d27e4905c0a40e3b9a660ee2bd72f9
Louis Lassegue <louislassegue@gmail.com>
2021-08-18 21:27:54 -0700

    Fixed bug.

--- src/middleware/attachUser.js
+++ src/middleware/attachUser.js
@@ -1,17 +1,15 @@
 const verifyToken = require('./verifiedToken.js')
 
-const { users } = require('../models/index.js')
+const dataModule = require('../models/index.js')
 
 module.exports = async (req, res, next) => {
   const token = req.headers.authorization.split(' ')[1]
-  const model = users.model
-  console.log('line 8', model)
+  const model = dataModule.users.model
 
   verifyToken(token, getUser)
 
   async function getUser (user) {
-    console.log('line 12', user)
-    const userRecord = await model.find({ where: { email: user.email } })
+    const userRecord = await model.findOne({ where: { email: user.email } })
     req.user = userRecord
     next()
   }
